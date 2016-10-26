bade463bdec071ce52b7620210c75aaabe06a249
sandflee <you@example.com>
2016-10-27 06:31:04 +0800

    struct

--- TestStruct.go
+++ TestStruct.go
@@ -4,6 +4,8 @@ import "fmt"
 
 type Person struct {
 	height int;
+	male bool;
+	label map[string]int;
 }
 
 func (person *Person) Add(num int)  {
@@ -16,11 +18,18 @@ func AddPersonHeight(p Person) {
 }
 
 func main() {
-	p := &Person{height:160}
+	p := &Person{height:160,label : map[string]int{"a":1}}
+	fmt.Println("%v",p)
 	fmt.Println(p.height)
 	p.Add(10)
 	fmt.Println(p.height)
 
 	AddPersonHeight(*p)
 	fmt.Println(p.height)
+
+	m := *p
+	m.label["a"] = 2
+	m.height = 0
+
+	fmt.Println(p)
 }
