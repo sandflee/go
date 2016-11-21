a4791e49843a9aa12d2f9d6792a53ac3ce969ce6
moonfang(房孝敬) <moonfang@tencent.com>
2016-11-21 16:11:51 +0800

    ss

--- TestSlice.go
+++ TestSlice.go
@@ -8,8 +8,28 @@ func modifySlice(num int , slice []int)  {
 	fmt.Printf("%+v", slice)
 }
 
-func main()  {
+type Person struct {
+	name string
+	age int
+}
+
+func main() {
 	a := []int{1,2,3,4}
 	modifySlice(8, a)
 	fmt.Printf("%+v", a)
+
+	s := make([]Person, 3)
+	s = append(s, Person{"a",1})
+	s = append(s, Person{"b",2})
+
+	for index, v := range s {
+		v.age = 10
+		fmt.Printf("%+v,%+v\n",index, v)
+	}
+
+	fmt.Printf("%+v\n", s)
+
+	var b []int
+	b = append(b, 1)
+	fmt.Printf("%+v\n", b)
 }
